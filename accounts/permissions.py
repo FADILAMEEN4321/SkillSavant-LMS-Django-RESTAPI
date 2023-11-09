14df704b2b9f6c6244d0005b3880aa7411e1d743
FADIL AMEEN V.M <129923924+FADILAMEEN4321@users.noreply.github.com>
2023-11-09 10:38:12 +0530

    feat: Implement custom user permissions for student, admin, and instructor
    This commit adds custom user permissions using base permissions class. Students, administrators, and instructors now have specific permissions defined for their roles.

--- accounts/permissions.py
+++ accounts/permissions.py
@@ -1,8 +1,22 @@
 from rest_framework import permissions
 
 
+class IsStudent(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if request.user.is_authenticated:
+            return request.user.role == 'student'
+        return False
+
+
 class IsInstructor(permissions.BasePermission):
     def has_permission(self, request, view):
         if request.user.is_authenticated:
             return request.user.role == 'instructor'
         return False 
+
+
+class IsAdminUser(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if request.user.is_authenticated:
+            return request.user.role == 'admin'
+        return False    
