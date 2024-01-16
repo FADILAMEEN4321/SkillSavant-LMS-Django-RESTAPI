2ba252b0230aa32f7862ca0a80196e2b754e9008
FADIL AMEEN V.M <129923924+FADILAMEEN4321@users.noreply.github.com>
2024-01-16 17:04:29 +0530

    fix: CSRF_TRUSTED_ORIGINS in settings.py

--- backend_lms/settings.py
+++ backend_lms/settings.py
@@ -113,7 +113,6 @@ MIDDLEWARE = [
 
     'django.contrib.sessions.middleware.SessionMiddleware',
 
-    # 'channels.middleware.ChannelsMiddleware',
 
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -207,6 +206,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -223,6 +224,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Cors settings
 CORS_ALLOW_ALL_ORIGINS = True
+
+CSRF_TRUSTED_ORIGINS = ['https://skillsavant.netlify.app/','https://skillsavant.online/','http://*.127.0.0.1']
 
 
 
