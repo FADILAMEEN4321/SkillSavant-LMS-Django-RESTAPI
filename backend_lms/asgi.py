651d79f01a37bca3a672ec37eab388e68c6dc44f
FADIL AMEEN V.M <129923924+FADILAMEEN4321@users.noreply.github.com>
2024-01-16 22:26:18 +0530

    fix: django channels issue.

--- backend_lms/asgi.py
+++ backend_lms/asgi.py
@@ -1,16 +1,3 @@
-
-# import os
-# from django.core.asgi import get_asgi_application
-
-# from django import setup
-
-
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend_lms.settings')
-# setup()
-
-# application = get_asgi_application()
-
-
 import os
 
 from django.core.asgi import get_asgi_application
--- backend_lms/routing.py
+++ backend_lms/routing.py
@@ -1,20 +1,3 @@
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
-# import chat.routing
-# from django.core.asgi import get_asgi_application
-
-
-# django_asgi_app = get_asgi_application()
-
-
-# application = ProtocolTypeRouter({
-#     "http": django_asgi_app,
-#     "websocket": 
-#         URLRouter(
-#             chat.routing.websocket_urlpatterns
-#         )
-# })
-
 from django.urls import re_path
 from chat.consumers import ChatRoomConsumer
 
